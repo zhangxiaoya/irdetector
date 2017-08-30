1521c168e0bd12b4d8647cf76412ec514687fc1f
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-08-30 11:01:23 +0800

    init and reset cuda device, add log printer

---
+++ DetectIRTargetGPU/Headers/GlobalMainHeaders.h
@@ -0,0 +1,11 @@
+#ifndef __GLOBAL_MAIN_HEADER__
+#define __GLOBAL_MAIN_HEADER__
+
+#include "cuda_runtime.h"
+#include "driver_types.h"
+#include "LogPrinter/LogPrinter.hpp"
+#include "Models/LogLevel.hpp"
+
+LogPrinter logPrinter;
+
+#endif __GLOBAL_MAIN_HEADER__
--- DetectIRTargetGPU/main.cpp
+++ DetectIRTargetGPU/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
 
+#include "Headers/GlobalMainHeaders.h"
+
+inline bool cudaDeviceInit(int argc, const char** argv)
+{
+	int deviceCount;
+	cudaGetDeviceCount(&deviceCount);
+
+	if (deviceCount == 0)
+	{
+		logPrinter.PrintLogs("CUDA error: no devices supporting CUDA.", LogLevel::Error);
+		exit(EXIT_FAILURE);
+	}
+
+	// Choose which GPU to run on, change this on a multi-GPU system.
+	auto cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess)
+	{
+		logPrinter.PrintLogs("cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?", LogLevel::Error);
+		return false;
+	}
+	logPrinter.PrintLogs("cudaSetDevice success!", LogLevel::Info);
+	return true;
+}
+
+void cudaDeviceRelease()
+{
+	auto cudaResetStatus = cudaDeviceReset();
+	if(cudaResetStatus == cudaSuccess)
+	{
+		logPrinter.PrintLogs("cudaDeviceReset success!", LogLevel::Info);
+	}
+	else
+	{
+		logPrinter.PrintLogs("cudaDeviceReset failed!", LogLevel::Waring);
+	}
+}
+
 int main(int argc, char* argv[])
 {
+	auto cudaInitStatus = cudaDeviceInit(argc, const_cast<const char **>(argv));
+	if(cudaInitStatus)
+	{
+		cudaDeviceRelease();
+	}
+
 	system("Pause");
 	return 0;
 }
