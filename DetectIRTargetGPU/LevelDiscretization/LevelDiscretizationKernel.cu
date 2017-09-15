a244d4ddfa78548290b80f99f054b06b24294c90
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-09-15 16:26:34 +0800

    name enhencement for level discretization kernel

--- DetectIRTargetGPU/LevelDiscretization/LevelDiscretizationKernel.cu
+++ DetectIRTargetGPU/LevelDiscretization/LevelDiscretizationKernel.cu
@@ -1,9 +1,9 @@
-
 #include "cuda_runtime.h"
 #include "LevelDiscretizationKernel.cuh"
 #include <device_launch_parameters.h>
+#include "../Checkers/CheckCUDAReturnStatus.h"
 
-__global__ void LevelDiscretizationKernel(unsigned char* frame, int width, int height, unsigned char scale)
+__global__ void LevelDiscretizationKernel(unsigned char* frameOnDevice, int width, int height, unsigned char scale)
 {
 	int x = blockDim.x * blockIdx.x + threadIdx.x;
 	int y = blockDim.y * blockIdx.y + threadIdx.y;
@@ -11,15 +11,16 @@ __global__ void LevelDiscretizationKernel(unsigned char* frame, int width, int h
 		return;
 
 	auto currentIdx = y * width + x;
-	frame[currentIdx] = (frame[currentIdx] / scale) * scale;
+	frameOnDevice[currentIdx] = (frameOnDevice[currentIdx] / scale) * scale;
 }
 
-void LevelDiscretizationOnGPU(unsigned char* image, int width, int height, int discretizationScale)
+void LevelDiscretizationOnGPU(unsigned char* frameOnDevice, int width, int height, int discretizationScale)
 {
+	auto status = true;
 	dim3 block(32, 32);
 	dim3 grid((width + 31) / 32, (height + 31) / 32);
 
-	LevelDiscretizationKernel<<<grid, block>>>(image, width, height, static_cast<unsigned char>(discretizationScale));
+	LevelDiscretizationKernel<<<grid, block>>>(frameOnDevice, width, height, static_cast<unsigned char>(discretizationScale));
 
-	auto cudaerr = cudaDeviceSynchronize();
+	CheckCUDAReturnStatus(cudaDeviceSynchronize(), status);
 }
