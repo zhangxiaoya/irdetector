65a10ffcd3d25a9678a4f1e33cadfe61f9813e6b
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-09-15 16:57:55 +0800

    remove old Mesh CCL kernle functions

--- DetectIRTargetGPU/CCL/MeshCCLKernelD.cuh
+++ DetectIRTargetGPU/CCL/MeshCCLKernelD.cuh
@@ -1,4 +1,3 @@
-#include <host_defines.h>
 #ifndef __MESH_KERNEL_D_H__
 #define __MESH_KERNEL_D_H__
 
@@ -6,15 +5,5 @@ const auto BlockX = 32;
 const auto BlockY = 32;
 
 void MeshCCL(unsigned char* frameOnDevice, int* labelsOnDevice, int width, int height);
-
-__device__ int IntMinOnDevice(int a, int b);
-
-__global__ void InitCCLOnDevice(int* labelsOnDevice, int* reference, int width, int height);
-
-__global__ void MeshKernelDScanning(unsigned char* frame, int* label, int* reference, const int width, const int height, bool* iterationFlag);
-
-__global__ void MeshKernelDAnalysis(int* label, int* reference, const int width, const int height);
-
-__global__ void MeshKernelDLabelling(int* label, int* reference, const int width, const int height);
 
 #endif
