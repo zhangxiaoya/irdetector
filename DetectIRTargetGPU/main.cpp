c71f2eba578c5921c105ee64b556cdc41e06b335
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-09-18 17:52:17 +0800

    test visual effect

--- DetectIRTargetGPU/main.cpp
+++ DetectIRTargetGPU/main.cpp
@@ -1,5 +1,6 @@
 #include "Validation/Validation.hpp"
 #include "Init/Init.hpp"
+#include "Validation/DetectorValidation.hpp"
 
 const unsigned int WIDTH = 320;
 const unsigned int HEIGHT = 256;
@@ -10,9 +11,13 @@ int main(int argc, char* argv[])
 	auto cudaInitStatus = CUDAInit::cudaDeviceInit();
 	if (cudaInitStatus)
 	{
-		Validation validation;
-		validation.InitValidationData("D:\\Cabins\\Projects\\Project1\\binaryFiles\\ir_file_20170531_1000m_1_partOne.bin");
-		validation.VailidationAll();
+//		Validation validation;
+//		validation.InitValidationData("D:\\Cabins\\Projects\\Project1\\binaryFiles\\ir_file_20170531_1000m_1_partOne.bin");
+//		validation.VailidationAll();
+
+		DetectorValidation validator;
+		validator.InitDataReader("D:\\Cabins\\Projects\\Project1\\binaryFiles\\ir_file_20170531_1000m_1_partOne.bin");
+		validator.VailidationAll();
 	}
 	CUDAInit::cudaDeviceRelease();
 
