0e95781f9f0cab0efc6a438bee95a38acd60df57
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-10-30 10:36:19 +0800

    remove unused codes in monitor

--- DetectIRTargetGPU/Monitor/Monitor.hpp
+++ DetectIRTargetGPU/Monitor/Monitor.hpp
@@ -1,48 +1,28 @@
 #ifndef __MONITOR_H__
 #define __MONITOR_H__
-#include <cstring>
-#include <cuda_runtime_api.h>
 
-// To-Do
 class Monitor
 {
 public:
-	Monitor(const int width, const int height):
-		originalFrameOnHost(nullptr),
-		originalFrameOnDevice(nullptr),
-		prepareResultOnHost(nullptr),
-		prepareResultOnDevice(nullptr),
-		Width(width),
-		Height(height)
+	Monitor(const int width, const int height): currentFrame(nullptr),
+	                                            Width(width),
+	                                            Height(height)
 	{
 	}
 
-	void ResetData(unsigned char* dataSource) const;
-
-	void Dialation();
+	bool Process(unsigned short* frame);
 
 private:
-	unsigned char* originalFrameOnHost;
-	unsigned char* originalFrameOnDevice;
-	unsigned char* prepareResultOnHost;
-	unsigned char* prepareResultOnDevice;
 
+	unsigned short* currentFrame;
 	int Width;
 	int Height;
 };
 
-inline void Monitor::ResetData(unsigned char* dataSource) const
+inline bool Monitor::Process(unsigned short* frame)
 {
-	if(dataSource !=nullptr)
-	{
-		memcpy(this->originalFrameOnHost, dataSource, sizeof(unsigned char) * Width * Height);
-		cudaMemcpy(originalFrameOnDevice, originalFrameOnHost, sizeof(unsigned char) * Width * Height, cudaMemcpyHostToDevice);
-	}
-}
 
-inline void Monitor::Dialation()
-{
-	// To-Do
+	return true;
 }
 
 #endif
