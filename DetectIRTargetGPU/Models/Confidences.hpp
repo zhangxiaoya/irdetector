81ff98c4d96de786230f9b031c9659fc798d895e
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-10-30 11:37:23 +0800

    add confidence map

---
+++ DetectIRTargetGPU/Models/Confidences.hpp
@@ -0,0 +1,57 @@
+#ifndef __CONFIDEBCES_H__
+#define __CONFIDEBCES_H__
+
+const int BlockSize = 16;
+
+typedef int ConfElem[6];
+
+class Confidences
+{
+public:
+	Confidences(const int width, const int height):
+		Width(width),
+		Height(height),
+		QueueBeg(0),
+		QueueEnd(0),
+		ConfidenceMap(nullptr)
+	{
+		BlockCols = (Width + (BlockSize - 1)) / BlockSize;
+		BlockRows = (Height + (BlockSize - 1)) / BlockSize;
+	}
+
+	~Confidences();
+
+	void InitConfidenceMap();
+
+	int BlockCols;
+	int BlockRows;
+	int Width;
+	int Height;
+
+	int QueueBeg;
+	int QueueEnd;
+
+	ConfElem** ConfidenceMap;
+};
+
+inline Confidences::~Confidences()
+{
+	for (auto i = 0; i < BlockRows; ++i)
+	{
+		delete[] ConfidenceMap[i];
+	}
+	delete[]  ConfidenceMap;
+}
+
+// NOT Thread Safe
+inline void Confidences::InitConfidenceMap()
+{
+	this->ConfidenceMap = new ConfElem*[BlockRows];
+	for (auto i = 0; i < BlockRows; ++i)
+	{
+		ConfidenceMap[i] = new ConfElem[BlockCols];
+	}
+	QueueBeg = QueueEnd = 0;
+}
+
+#endif
--- DetectIRTargetGPU/Monitor/Monitor.hpp
+++ DetectIRTargetGPU/Monitor/Monitor.hpp
@@ -1,28 +1,45 @@
 #ifndef __MONITOR_H__
 #define __MONITOR_H__
+#include "../Models/Confidences.hpp"
 
 class Monitor
 {
 public:
 	Monitor(const int width, const int height): currentFrame(nullptr),
 	                                            Width(width),
-	                                            Height(height)
+	                                            Height(height),
+	                                            confidences(nullptr)
 	{
+		confidences = new Confidences(Width, Height);
+	}
+
+	~Monitor()
+	{
+		delete confidences;
 	}
 
 	bool Process(unsigned short* frame);
 
-private:
+protected:
+	void InitMonitor();
 
+private:
 	unsigned short* currentFrame;
 	int Width;
 	int Height;
+
+	Confidences* confidences;
 };
 
 inline bool Monitor::Process(unsigned short* frame)
 {
 
 	return true;
+}
+
+inline void Monitor::InitMonitor()
+{
+	confidences->InitConfidenceMap();
 }
 
 #endif
