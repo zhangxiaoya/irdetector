73af00b642fe187f2d69b4e5df7ce9a85e6d6df4
nobody <nobody@somewhere.com>
2017-12-05 18:58:33 +0800

    finish trace

--- DetectIRTargetGPU/Models/Trace.hpp
+++ DetectIRTargetGPU/Models/Trace.hpp
@@ -2,13 +2,19 @@
 #include "../Headers/SearcherParameters.h"
 #include "TraceItem.hpp"
 
+#ifndef MAX_TRACE_LIFE_TIME
+#define MAX_TRACE_LIFE_TIME (10)
+#endif // !MAX_TRACE_LIFE_TIME
+
 struct Trace
 {
 public:
 	Trace() :
 		queueFront(0),
 		queueEnd(0),
-		LifeTime(0)
+		LifeTime(0),
+		FrameIndex(-1),
+		InterIndex(-1)
 	{
 	}
 
@@ -17,8 +23,9 @@ public:
 	{
 		TraceQueue[queueEnd] = _item;
 		queueEnd++;
+
 		// 队列满,删除队列头元素
-		if (queueEnd == queueFront)
+		if (TraceQueueIsFull())
 		{
 			queueFront++;
 		}
@@ -27,8 +34,9 @@ public:
 	// 出队列
 	void OutQueue()
 	{
-		if (queueFront == queueEnd)
+		if (TraceQueueIsEmpty())
 		{
+			ShrinkLifeTime();
 			return;
 		}
 		else
@@ -37,9 +45,51 @@ public:
 		}
 	}
 
+	// 延长Trace生命时间
+	void ExtendLifeTime()
+	{
+		LifeTime++;
+		if (LifeTime > MAX_TRACE_LIFE_TIME)
+		{
+			LifeTime = MAX_TRACE_LIFE_TIME;
+		}
+	}
+
+	// 缩减Trace生命时间
+	void ShrinkLifeTime()
+	{
+		LifeTime--;
+		if (LifeTime < 0)
+		{
+			LifeTime = 0;
+		}
+	}
+
+	// 获取轨迹的长度
+	int TraceLength()
+	{
+		return (queueEnd - queueFront + MAX_TRACE_QUEUE_LENGTH + 1) % (MAX_TRACE_QUEUE_LENGTH + 1);
+	}
+
+	// 判断Trace队列是否为空
+	bool TraceQueueIsEmpty()
+	{
+		return queueEnd == queueFront;
+	}
+
+	// 判断Trace队列是否满
+	bool TraceQueueIsFull()
+	{
+		return (queueFront + 1) % (MAX_TRACE_QUEUE_LENGTH + 1) == queueEnd;
+	}
+
 private:
 	int queueFront;
 	int queueEnd;
 	int LifeTime;
+
+	int FrameIndex;
+	int InterIndex;
+
 	TraceItem TraceQueue[MAX_TRACE_QUEUE_LENGTH + 1];
 };
